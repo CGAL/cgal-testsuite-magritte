de05be3ef7904e2b660eeeb8bcc63dcb69c63b12
magritte <magritte@magritte.home>
2024-11-25 10:41:40 +0100

    cleanup after testsuite

--- run_testsuite.sh
+++ run_testsuite.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 cd /Users/magritte/cgal_root
 PATH=/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin && /bin/bash ./autotest_cgal_with_ctest -s > /Users/magritte/cgal_root/cronlogs 2>&1 
+
+tgz=`cat RELEASE_NR`
+echo rm  $tgz
+res_dir=`echo $tgz | sed 's/.tar.gz//'`
+echo rm -r $res_dir
+
